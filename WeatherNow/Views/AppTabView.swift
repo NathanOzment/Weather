f2c13571390c5aaab22616b8b2bd17bf0a2db608
NathanOzment <166570392+NathanOzment@users.noreply.github.com>
2026-04-26 19:38:20 -0500

    Refine tab bar appearance

--- WeatherNow/Views/AppTabView.swift
+++ WeatherNow/Views/AppTabView.swift
@@ -39,5 +39,29 @@ struct AppTabView: View {
         .tint(Color(red: 0.99, green: 0.84, blue: 0.42))
         .toolbarBackground(.visible, for: .tabBar)
         .toolbarColorScheme(.dark, for: .tabBar)
+        .task {
+            configureTabBarAppearance()
+        }
+    }
+
+    private func configureTabBarAppearance() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundEffect = UIBlurEffect(style: .systemUltraThinMaterialDark)
+        appearance.backgroundColor = UIColor(red: 0.10, green: 0.14, blue: 0.24, alpha: 0.62)
+        appearance.shadowColor = UIColor.white.withAlphaComponent(0.06)
+
+        let selectedColor = UIColor(red: 0.99, green: 0.84, blue: 0.42, alpha: 1.0)
+        let normalColor = UIColor.white.withAlphaComponent(0.82)
+
+        for itemAppearance in [appearance.stackedLayoutAppearance, appearance.inlineLayoutAppearance, appearance.compactInlineLayoutAppearance] {
+            itemAppearance.selected.iconColor = selectedColor
+            itemAppearance.selected.titleTextAttributes = [.foregroundColor: selectedColor]
+            itemAppearance.normal.iconColor = normalColor
+            itemAppearance.normal.titleTextAttributes = [.foregroundColor: normalColor]
+        }
+
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
     }
 }
